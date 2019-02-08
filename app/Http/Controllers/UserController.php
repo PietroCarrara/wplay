b97737afdf6d005663bef9e76e826d3c27262ae1
Pietro Carrara <piticarrara@gmail.com>
2019-02-08 16:12:48 -0200

    Filtrar relatório de usuário por data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 use Auth;
 use Gate;
 use \App\User;
@@ -74,7 +75,7 @@ class UserController extends Controller
         return redirect()->back();
     }
 
-    public function report($id) {
+    public function report(Request $req, $id) {
 
         $user = User::find($id);
 
@@ -86,8 +87,39 @@ class UserController extends Controller
             return redirect()->back()->withErrors('Você não tem permissão para isso');
         }
 
+        $req->validate([
+            'start' => 'date|nullable',
+            'end' => 'date|nullable'
+        ]);
+
+        $start = null;
+        $end = null;
+        if ($req->input('start') != '') {
+            $start = new Carbon($req->input('start'));
+        }
+        if ($req->input('end') != '') {
+            $end = new Carbon($req->input('end'));
+
+            // O final é o FIM do dia informado
+            $end->hour = 23;
+            $end->minute = 59;
+            $end->second = 59;
+        }
+
+        if ($start && $end && $end < $start) {
+            return redirect()->back()->withErrors('Invalid dates informed');
+        }
+
+        // Filtrando pontos batidos do usuário
+        $userControls = $user->controls();
+        if ($start) {
+            $userControls = $userControls->where('created_at', '>=', $start);
+        }
+        if ($end) {
+            $userControls = $userControls->where('created_at', '<=', $end);
+        }
         $controls = [];
-        foreach($user->controls as $control) {
+        foreach($userControls->where('type', 'login')->get() as $control) {
             // Vamos supor que um login antes das 10:00 da manhã seja um ponto batido
             if ($control->created_at->hour < 10) {
                 // Vamos contar esse dia nos pontos batidos
@@ -97,9 +129,43 @@ class UserController extends Controller
         // Múltiplos logins no mesmo dia são só um ponto batido
         $controls = array_unique($controls);
 
+        // Filtrando comentários
+        $comms = $user->comments();
+        if ($start) {
+            $comms = $comms->where('created_at', '>=', $start);
+        }
+        if ($end) {
+            $comms = $comms->where('created_at', '<=', $end);
+        }
+        $comms = $comms->get();
+
+
+        // Filtrando os logs
+        $logs = $user->logs();
+        if ($start) {
+            $logs = $logs->where('created_at', '>=', $start);
+        }
+        if ($end) {
+            $logs = $logs->where('created_at', '<=', $end);
+        }
+
+        // Filtrando as tarefas completadas
+        $tasks = $user->tasks()->onlyTrashed();
+        if ($start) {
+            $tasks = $tasks->where('deleted_at', '>=', $start);
+        }
+        if ($end) {
+            $tasks = $tasks->where('deleted_at', '<=', $end);
+        }
+
         return view('user-report', [
             'user' => $user,
             'controls' => $controls,
+            'logs' => $logs,
+            'comments' => $comms,
+            'tasks' => $tasks,
+            'start' => $start,
+            'end' => $end,
         ]);
     }
 }
--- resources/views/user-report.blade.php
+++ resources/views/user-report.blade.php
@@ -8,6 +8,35 @@
                 Relatório de: {{ $user->name }}
             </div>
             <div class="card-body">
+                {{-- Filtros de data --}}
+                <h3>Filtrar Período</h3>
+                <form>
+                    <div class="row mb-4">
+                        <div class="col-md-12">
+                            @include('components.errors')
+                        </div>
+                        <div class="form-row mx-3">
+                            <div class="form-group col-md-6">
+                                <label for="startDate">Início</label>
+                                <input type="date" id="startDate" name="start" class="form-control"
+                                @if ($start)
+                                    value="{{ $start->format('Y-m-d') }}"
+                                @endif>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="endDate">Fim</label>
+                                <input type="date" id="endDate" name="end" class="form-control"
+                                @if ($end)
+                                    value="{{ $end->format('Y-m-d') }}"
+                                @endif>
+                            </div>
+                            <div class="form-group col-md-12">
+                                <small>Ambos os filtros são opcionais. Informe somente um para visualisar dados a partir de/até certa data.</small><br/>
+                            </div>
+                            <button class="btn btn-primary" type="submit">Filtrar</button>
+                        </div>
+                    </div>
+                </form>
                 {{-- Infos do usuário --}}
                 <h3>Informações</h3>
                 <div class="row mb-4">
@@ -19,10 +48,10 @@
                             count($controls) }}</a>
                     </div>
                     <div class="col-md-6 my-2">
-                        <strong>Total de comentários:</strong> {{ $user->comments()->get()->count() }}
+                        <strong>Total de comentários:</strong> <a href="#" data-toggle="modal" data-target="#commentModal">{{ $comments->count() }}</a>
                     </div>
                     <div class="col-md-6 my-2">
-                        <strong>Total de tarefas completadas:</strong> {{ $user->tasks()->onlyTrashed()->get()->count()
+                        <strong>Total de tarefas completadas:</strong> {{ $tasks->count()
                         }}
                     </div>
                 </div>
@@ -31,7 +60,7 @@
                 <h3>Atividades</h3>
                 <div class="row">
                     @php($tmpDate = '')
-                    @foreach($user->logs()->orderBy('created_at')->get() as $log)
+                    @foreach($logs->orderBy('created_at')->get() as $log)
                     @if ($tmpDate != $log->created_at->format('d/m'))
                     @php($tmpDate = $log->created_at->format('d/m'))
                     <div class="col-md-12 text-center">
@@ -50,11 +79,33 @@
         </div>
     </div>
 </div>
+<div class="modal fade" id="commentModal" tabindex="-1" role="dialog" aria-labelledby="commentModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="commentModalLabel">Comentários</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body row">
+                @foreach($comments as $comm)
+                <div class="col-md-7 my-2">
+                    <strong>{{ $comm->user->name }}</strong>: {{ $comm->contents }}<br/>
+                </div>
+                <div class="col-md-5 my-2 text-right">
+                    Na tarefa @include('components.task-link', ['task' => $comm->task()->withTrashed()->get()->first()]), em {{ $comm->created_at->format('d/m/y') }}
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
 <div class="modal fade" id="controlModal" tabindex="-1" role="dialog" aria-labelledby="controlModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Dias de Ponto Batido</h5>
+                <h5 class="modal-title" id="controlModalLabel">Dias de Ponto Batido</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
